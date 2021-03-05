0cb00bc3603139b40ffad6169254e774c879fd20
Valter Rosenfeld <valter.rosenfeld@tigu.hk.tlu.ee>
2021-03-05 15:48:09 +0200

    Esimese tunni esimene pool

---
+++ page.php
@@ -0,0 +1,33 @@
+<?php
+	$myname = "Valter Rosenfeld";
+	$currenttime = date("d.m.Y H:i:s");
+	$timehtml = "\n <p>Lehe avamise hetkel oli aeg: " .$currenttime . ". </p>";
+	$semesterbegin = new DateTime("2021-1-25");
+	$semesterend = new DateTime("2021-6-30");
+	$semesterduration = $semesterbegin->diff($semesterend);
+	$semesterdurationdays = $semesterduration->format("%r%a");
+	$semesterdurhtml = "\n <p>2021 kevadsemestri kestus on " .$semesterdurationdays ." päeva.</p> \n";
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>Veebirakendused ja nende loomine 2021</title>
+</head>
+<body>
+	<h1>
+	<?php
+		echo $myname;
+		
+	?>
+	
+	</h1>
+	<p>See leht on valminud õppetöö raames!</p>
+	<?php
+		echo $timehtml;
+		echo $semesterdurhtml;
+	?>
+</body>
+</html>
